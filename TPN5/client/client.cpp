cd6c4ea4dec25d6c0840d6399b8fefe437e6b617
EllySp <elly.indra.esparza@mi.unc.edu.ar>
2025-05-25 13:37:45 -0300

    add TPN5

---
+++ TPN5/client/client.cpp
@@ -0,0 +1,123 @@
+/**
+ * Main file for client app
+ */
+
+#include <netinet/in.h>
+#include <iostream>
+#include <unistd.h>
+#include <bits/ostream.tcc>
+#include <netdb.h>
+#include <strings.h>
+#include <csignal>
+#include <vector>
+
+/// Socket file descriptor to communicate whit the server
+int sock_fd = -1;
+
+/**
+ * signal handler used to force program exit
+ */
+void sig_handler(int signum)
+{
+	if (sock_fd > 0)
+	{
+		close(sock_fd);
+	}
+	sock_fd = -1;
+}
+
+/**
+ * @param argc number of arguments
+ * @param argv client <host ipv6> <port> <n° of iterations> <ms between messages>
+ */
+int main(const int argc, char *argv[])
+{
+	// Verify arguments
+	if (argc < 5)
+	{
+		std::cerr << "Usage: " << argv[0] << " <host> <port> <iterations> <delta time ms>" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Setup signal handlers
+	signal(SIGINT, sig_handler);
+	signal(SIGTERM, sig_handler);
+
+	// Find server address info using host and port arguments
+	addrinfo hints{}, *res;
+	//hints.ai_family = AF_INET6;
+	hints.ai_socktype = SOCK_STREAM;
+	if (getaddrinfo(argv[1], argv[2], &hints, &res) != 0 || res == nullptr)
+	{
+		std::cerr << "Failed to resolve host/port" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Create a socket to communicate with the server
+	sock_fd = socket(res->ai_family, res->ai_socktype, 0);
+	if (sock_fd < 0)
+	{
+		std::cerr << "Failed to create socket" << std::endl;
+		freeaddrinfo(res);
+		return EXIT_FAILURE;
+	}
+
+	// Connect to server
+	if (connect(sock_fd, res->ai_addr, res->ai_addrlen) < 0)
+	{
+		if (sock_fd > 0)
+		{
+			close(sock_fd);
+		}
+		freeaddrinfo(res);
+		std::cerr << "Failed to connect to server" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	const auto iterations = std::stoi(argv[3]);
+	const auto sleep_ms = std::stoi(argv[4]);
+	const std::string preamble = "WI-Fighters-";
+	std::string msg{};
+
+	// Begin communications
+	for (auto i = 0; i < iterations; ++i)
+	{
+		// First send a message (WI_Fighters-i)
+		msg = preamble + std::to_string(i);
+		if (write(sock_fd, msg.data(), msg.size()) < 0)
+		{
+			if (sock_fd != -1)
+				close(sock_fd);
+			freeaddrinfo(res);
+			std::cerr << "Failed to write to server" << std::endl;
+			return EXIT_FAILURE;
+		}
+
+		// Then receive a successful response
+		std::vector<char> buffer(1024);
+		if (read(sock_fd, buffer.data(), 1024) <= 0)
+		{
+			if (sock_fd != -1)
+				close(sock_fd);
+
+			freeaddrinfo(res);
+			std::cerr << "Failed to read from server" << std::endl;
+			return EXIT_FAILURE;
+		}
+		std::cout << "SERVER: " << buffer.data() << std::endl;
+
+		// Sleep
+		usleep(sleep_ms * 1000);
+	}
+
+	if (sock_fd != -1)
+	{
+		// Send the "quit" message to the server before exiting
+		msg = "quit";
+		write(sock_fd, msg.data(), msg.size());
+		close(sock_fd);
+	}
+	freeaddrinfo(res);
+
+	return EXIT_SUCCESS;
+}
---
+++ TPN5/server/server.cpp
@@ -0,0 +1,137 @@
+/**
+ * Mian file for the server app
+ */
+
+#include <netinet/in.h>
+#include <iostream>
+#include <unistd.h>
+#include <csignal>
+#include <vector>
+
+/// Socket file descriptor of the server
+int sock_fd = -1;
+
+/**
+ * signal handler used to force program exit
+ */
+void sig_handler(int signum)
+{
+	if (sock_fd > 0)
+	{
+		close(sock_fd);
+	}
+	sock_fd = -1;
+}
+
+/**
+ * @param argc number of arguments
+ * @param argv server <port>
+ */
+int main(const int argc, char* argv[])
+{
+	// Verify arguments
+	if (argc < 2)
+	{
+		std::cerr << "Usage: " << argv[0] << " <port>" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Setup signal handlers
+	signal(SIGINT, sig_handler);
+	signal(SIGTERM, sig_handler);
+
+	// Create a socket for the server
+	sock_fd = socket(AF_INET6, SOCK_STREAM, 0);
+	if (sock_fd < 0)
+	{
+		std::cerr << "Failed to create socket" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Set socket options so the server can accept both IPv4 and IPv6 connections
+	if (constexpr int disable{}; setsockopt(sock_fd, IPPROTO_IPV6, IPV6_V6ONLY, &disable, sizeof(disable)) < 0)
+	{
+		if (sock_fd != -1)
+			close(sock_fd);
+		std::cerr << "Failed to set socket option" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Bind socket to address/port
+	sockaddr_in6 addr{};
+	addr.sin6_family = AF_INET6;
+	addr.sin6_port = htons(std::stoi(argv[1]));
+	addr.sin6_addr = in6addr_any;
+	if (bind(sock_fd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr)) < 0)
+	{
+		if (sock_fd != -1)
+			close(sock_fd);
+		std::cerr << "Failed to bind socket" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Setup max number of connections
+	if (listen(sock_fd, 1) < 0)
+	{
+		if (sock_fd != -1)
+			close(sock_fd);
+		std::cerr << "Failed to listen" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	// Begin passive open (accept)
+	sockaddr_in6 client_addr{};
+	socklen_t client_addr_len = sizeof(client_addr);
+	const int client_fd = accept(sock_fd, reinterpret_cast<sockaddr*>(&client_addr), &client_addr_len);
+	if (client_fd < 0)
+	{
+		if (sock_fd != -1)
+			close(sock_fd);
+		std::cerr << "Failed to accept connection" << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::string msg{};
+	while (true)
+	{
+		// Wait for the client to send a message
+		std::vector<char> buffer(1024);
+		if (read(client_fd, buffer.data(), 1024) <= 0)
+		{
+			if (sock_fd != -1)
+				close(sock_fd);
+			close(client_fd);
+			std::cerr << "Failed to read from client" << std::endl;
+			return EXIT_FAILURE;
+		}
+		msg = {buffer.data()};
+
+		// TODO: log received message
+		std::cout << "CLIENT: " << msg << std::endl;
+
+		// If the message is "quit" finish
+		if (msg == "quit")
+		{
+			std::cout << "Client disconnected" << std::endl;
+			break;
+		}
+
+		// Response
+		msg = "success";
+		if (write(client_fd, msg.data(), msg.size()) < 0)
+		{
+			if (sock_fd != -1)
+				close(sock_fd);
+			close(client_fd);
+			std::cerr << "Failed to write to client" << std::endl;
+			return EXIT_FAILURE;
+		}
+		// TODO: log sent message
+	}
+
+	if (sock_fd != -1)
+		close(sock_fd);
+	close(client_fd);
+	std::cout << "Closing server" << std::endl;
+	return EXIT_SUCCESS;
+}
